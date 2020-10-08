61ea1851e64c387bf56c58db760fc3d5970ccc86
OKAUEND <def1f23sbb@gmail.com>
2020-10-08 19:20:09 +0900

    Add - チュートリアルに従い各クラスをつくる

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+class Square extends React.Component {
+  render() {
+    return {};
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i) {}
+
+  render() {
+    return {};
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return {};
+  }
+}
+
+//====================================
+ReactDOM.render(<Game />, document.getElementById("root"));
