ae9b5390366f377487228063107f3e303874fee1
OKAUEND <def1f23sbb@gmail.com>
2020-10-09 16:53:34 +0900

    Add - チュートリアエウに従い、各コンポーネントの中身を記述する

--- src/index.js
+++ src/index.js
@@ -4,21 +4,54 @@ import "./index.css";
 
 class Square extends React.Component {
   render() {
-    return {};
+    return <button className="squaer">{this.props.value}</button>;
   }
 }
 
 class Board extends React.Component {
-  renderSquare(i) {}
+  renderSquare(i) {
+    return <Square value={i} />;
+  }
 
   render() {
-    return {};
+    const status = "Next `player X";
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          <div>{this.renderSquare("Hage")}</div>
+          <div>{this.renderSquare(1)}</div>
+          <div>{this.renderSquare(2)}</div>
+        </div>
+        <div className="board-row">
+          <div>{this.renderSquare(3)}</div>
+          <div>{this.renderSquare(4)}</div>
+          <div>{this.renderSquare(5)}</div>
+        </div>
+        <div className="board-row">
+          <div>{this.renderSquare(6)}</div>
+          <div>{this.renderSquare(7)}</div>
+          <div>{this.renderSquare(8)}</div>
+        </div>
+      </div>
+    );
   }
 }
 
 class Game extends React.Component {
   render() {
-    return {};
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/*status*/}</div>
+          <div>{/* TODO */}</div>
+        </div>
+      </div>
+    );
   }
 }
 
